3169375a2d6213a4495193488d68ec61c32ea76a
Konrad-Rejman <u09kr22@abdn.ac.uk>
2026-02-12 13:14:36 +0000

    Basic full context game loop added

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ### By Konrad Rejmanowski
 
+To run the code, you will need to add a .env file with HOSTURL set to the site hosting your LLMs.
--- main.py
+++ main.py
@@ -6,15 +6,31 @@ load_dotenv()
 client = ollama.Client(host=os.getenv('HOSTURL'))
 
 # Models: ['qwen3:32b', 'gemma3:12b', 'deepseek-r1:32b', 'gpt-oss:120b', 'llama3.1:8b', 'llama3.2:latest', 'gpt-oss:latest']
-model = 'qwen3:32b'
+model = 'gpt-oss:120b'
 
-# Get user input
-input = input('Enter a starting prompt: ')
-print('Input:\n' + input)
+# Game start
+print('Press ctrl + c to exit.')
+print('The LLM will act as the Game Master, play along by inputing your characters actions each turn and the LLM will respond with the outcome setting up the next turn.')
+print('Generating...')
 
-# Get response from model
-response = client.chat(model=model, messages=[ 
-    {'role': 'user',  'content': input}, # User input for model to respond to
-    {'role': 'system',  'content': 'Act rude in your responses.'}, # Instructions to model on how to act
-]) 
-print('Response:\n' + response.message.content)
+rules = {'role': 'system', 'content': 'Act as the GameMaster for the following pen and paper game, with the user acting as player from now on. Resolve the outcome of player actions by simulating a dice roll for the player.'}
+scenario = {'role': 'user', 'content': 'Describe a start for the following scenario: the player wakes up on a forest road with no memories, they are beside a caravan which has been destroyed, a trail leads from the wreckage into the forest whilst the road leads out of the forest.'}
+
+startMessage = client.chat(model=model, messages=[
+    rules,
+    scenario,
+])
+print('GM:\n' + startMessage.message.content)
+
+chatlogs = [rules, {'role': 'assistant', 'content': startMessage.message.content}]
+
+while True:
+    action = input('Describe the players\' actions: ')
+    print('Player:\n' + action)
+    chatlogs.append({'role': 'user',  'content': action}) # Add Player input to chat history
+
+    # Get response from model
+    response = client.chat(model=model, messages=chatlogs)
+    chatlogs.append({'role': 'assistant',  'content': response.message.content}) # Add GM response to chat history
+
+    print('GM:\n' + response.message.content)
