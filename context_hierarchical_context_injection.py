e6dd5d9d5bfdbd0859c26aa2ba931da255976a39
Konrad-Rejman <u09kr22@abdn.ac.uk>
2026-03-09 15:46:49 +0000

    Changed try-except implementation

--- context_hierarchical_context_injection.py
+++ context_hierarchical_context_injection.py
@@ -4,32 +4,33 @@ def hierarchical_context(chatlogs, rules, save, client, model, hierarchical_summ
     try:
         action = input('\nDescribe the players\' actions: ')
         chatlogs.append({'role': 'user',  'content': action}) # Add Player input to chat history
+
+        # Generate random rolls for model to use
+        rolls = {'role': 'system', 'content': 'Use the following random rolls for this interaction as needed: '}
+        roll_num = 5 # Number of random rolls to pass to model
+        for i in range(roll_num): 
+            r = random.randint(1, 20)
+            rolls['content'] = rolls['content'] + str(r)
+            if i < roll_num - 1:
+                rolls['content'] = rolls['content'] + ', '
+
+        # Get response from model
+        memory = [rules, rolls, {'role': 'system', 'content': 'This is an overview of the story so far: ' + hierarchical_summary}, {'role': 'user',  'content': action}]
+        response = client.chat(model=model, messages=memory)
+        tokens += response.prompt_eval_count # Add tokens processed to token counter
+        chatlogs.append({'role': 'assistant',  'content': response.message.content}) # Add GM response to chat history
+
+        print('GM:\n' + response.message.content)
+
+        # Update the summary based on most recent context
+        instructions = {'role': 'system', 'content': 'Update the following Summary without removing its current headings or changing its current structure (OVERALL STORY, CURRENT QUEST, PLAYER STATUS): ' + hierarchical_summary}
+        memory = [instructions, {'role': 'user',  'content': action}]
+        new_hierarchical_summary = client.chat(model=model, messages=memory)
+        tokens += new_hierarchical_summary.prompt_eval_count # Add tokens processed to token counter
+        hierarchical_summary = new_hierarchical_summary.message.content
+
     except KeyboardInterrupt:
         save() # Save session data
         quit() # End program
-
-    # Generate random rolls for model to use
-    rolls = {'role': 'system', 'content': 'Use the following random rolls for this interaction as needed: '}
-    roll_num = 5 # Number of random rolls to pass to model
-    for i in range(roll_num): 
-        r = random.randint(1, 20)
-        rolls['content'] = rolls['content'] + str(r)
-        if i < roll_num - 1:
-            rolls['content'] = rolls['content'] + ', '
-
-    # Get response from model
-    memory = [rules, rolls, {'role': 'system', 'content': 'This is an overview of the story so far: ' + hierarchical_summary}, {'role': 'user',  'content': action}]
-    response = client.chat(model=model, messages=memory)
-    tokens += response.prompt_eval_count # Add tokens processed to token counter
-    chatlogs.append({'role': 'assistant',  'content': response.message.content}) # Add GM response to chat history
-
-    print('GM:\n' + response.message.content)
-
-    # Update the summary based on most recent context
-    instructions = {'role': 'system', 'content': 'Update the following Summary without removing its current headings or changing its current structure (OVERALL STORY, CURRENT QUEST, PLAYER STATUS): ' + hierarchical_summary}
-    memory = [instructions, {'role': 'user',  'content': action}]
-    new_hierarchical_summary = client.chat(model=model, messages=memory)
-    tokens += new_hierarchical_summary.prompt_eval_count # Add tokens processed to token counter
-    hierarchical_summary = new_hierarchical_summary.message.content
 
     return tokens, hierarchical_summary
--- context_running_summary.py
+++ context_running_summary.py
@@ -4,32 +4,33 @@ def running_summary(chatlogs, rules, save, client, model, summary, tokens):
     try:
         action = input('\nDescribe the players\' actions: ')
         chatlogs.append({'role': 'user',  'content': action}) # Add Player input to chat history
+
+        # Generate random rolls for model to use
+        rolls = {'role': 'system', 'content': 'Use the following random rolls for this interaction as needed: '}
+        roll_num = 5 # Number of random rolls to pass to model
+        for i in range(roll_num): 
+            r = random.randint(1, 20)
+            rolls['content'] = rolls['content'] + str(r)
+            if i < roll_num - 1:
+                rolls['content'] = rolls['content'] + ', '
+
+        # Get response from model
+        memory = [rules, rolls, {'role': 'system', 'content': 'This is an overview of the story so far: ' + summary}, {'role': 'user',  'content': action}]
+        response = client.chat(model=model, messages=memory)
+        tokens += response.prompt_eval_count # Add tokens processed to token counter
+        chatlogs.append({'role': 'assistant',  'content': response.message.content}) # Add GM response to chat history
+
+        print('GM:\n' + response.message.content)
+
+        # Update the summary based on most recent context
+        instructions = {'role': 'system', 'content': 'Update the following Summary without removing the capitalised heading: ' + summary}
+        memory = [instructions, {'role': 'user',  'content': action}]
+        new_summary = client.chat(model=model, messages=memory)
+        tokens += new_summary.prompt_eval_count # Add tokens processed to token counter
+        summary = new_summary.message.content
+    
     except KeyboardInterrupt:
         save() # Save session data
         quit() # End program
-
-    # Generate random rolls for model to use
-    rolls = {'role': 'system', 'content': 'Use the following random rolls for this interaction as needed: '}
-    roll_num = 5 # Number of random rolls to pass to model
-    for i in range(roll_num): 
-        r = random.randint(1, 20)
-        rolls['content'] = rolls['content'] + str(r)
-        if i < roll_num - 1:
-            rolls['content'] = rolls['content'] + ', '
-
-    # Get response from model
-    memory = [rules, rolls, {'role': 'system', 'content': 'This is an overview of the story so far: ' + summary}, {'role': 'user',  'content': action}]
-    response = client.chat(model=model, messages=memory)
-    tokens += response.prompt_eval_count # Add tokens processed to token counter
-    chatlogs.append({'role': 'assistant',  'content': response.message.content}) # Add GM response to chat history
-
-    print('GM:\n' + response.message.content)
-
-    # Update the summary based on most recent context
-    instructions = {'role': 'system', 'content': 'Update the following Summary without removing the capitalised heading: ' + summary}
-    memory = [instructions, {'role': 'user',  'content': action}]
-    new_summary = client.chat(model=model, messages=memory)
-    tokens += new_summary.prompt_eval_count # Add tokens processed to token counter
-    summary = new_summary.message.content
 
     return tokens, summary
